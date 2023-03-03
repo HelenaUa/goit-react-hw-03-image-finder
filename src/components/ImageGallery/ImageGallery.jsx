5958a1586f60a2c880ee36d4b77d6ec0dcccde0f
HelenaUa <delena.1408@gmail.com>
2023-03-03 20:52:29 +0200

    add components new

--- src/components/ImageGallery/ImageGallery.jsx
+++ src/components/ImageGallery/ImageGallery.jsx
@@ -4,6 +4,7 @@ import { ImageGalleryUl } from './ImageGallery.styled';
 export const ImageGallery = () => {
     return (
         <ImageGalleryUl>
+            <ImageGalleryItem />
            {/* <!-- Набор <li> с изображениями --> */}
         </ImageGalleryUl>
     )
